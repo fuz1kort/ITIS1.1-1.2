010e04e15fdcdae8346102daf384b3f1ef8f47b4
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2022-09-20 13:38:37 +0300

    Синус в синусе

--- myrep/ls1/Program.cs
+++ myrep/ls1/Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("Здарова");
-Console.WriteLine("АНИМЕЕЕЕЕ");
+﻿double x = double.Parse(Console.ReadLine()), y = 0;
+int k = int.Parse(Console.ReadLine());
+double p = x;
+while (k > 0)
+{
+    y += Math.Sin(p);
+    k--;
+    p = Math.Sin(p);
+}
+Console.WriteLine(y);
+//Корни 
