b6ab8556c26e0de9d5a00412e4407c0562c6e9ff
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2022-11-23 20:16:59 +0300

    v 1.0

---
+++ myrep/OOP/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FractionApp
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //try
+            //{
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(32);
+            Fraction f3 = new Fraction();
+
+            f1.Numerator = 3;
+            //f1.SetDenom(2);
+
+
+            Fraction.StaticMethod();
+
+
+            //f1.Print();
+            Console.WriteLine(f1);
+
+
+            //f2.SetNum(2);
+            f2.Denominator = 1;
+
+            //Console.WriteLine($"Fraction - {f2.Numerator}/{f2.Denominator}");
+            Console.WriteLine($"Fraction - {f2}");
+
+
+            //f3.Print();
+            Console.WriteLine(f3);
+
+            f1.Add(f2);
+            //f1.Print();
+            Console.WriteLine(f1);
+
+            f3.Add(f3, f2);
+            //f3 = f3 + f2;
+            //f3.Print();
+            Console.WriteLine(f3);
+
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+        }
+
+    }
+
+}
---
+++ myrep/time/Time.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace time
+{
+    class Time
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        public Time() { hour = 0; minute = 0; second = 0; }
+
+        public Time(int second) 
+        { 
+            Second = second;
+        }
+        public Time(int hour, int minute) 
+        { 
+            Hour = hour; 
+            Minute = minute;
+        }
+        public Time(int hour, int minute, int second) 
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public int Hour
+        {
+            get { return hour; }
+            set
+            {
+                if (value > 23)
+                {
+                    Console.WriteLine("Часы были скорректированы");
+                    hour = value % 24;
+                }
+                else hour = value;
+            }
+        }
+
+        public int Minute
+        {
+            get { return minute; }
+            set
+            {
+                if (value >= 60)
+                {
+                    Console.WriteLine("Минуты были скорректированы");
+                    minute = value % 60;
+                    Hour += value / 60;
+                }
+                else minute = value;
+            }
+        }
+
+        public int Second
+        {
+            get { return second; }
+            set
+            {
+                if (value >= 60)
+                {
+                    Console.WriteLine("Секунды были скорректированы");
+                    second = value % 60;
+                    Minute += value / 60;
+                    Hour += Minute / 60;
+                }
+                else second = value;
+            }
+        }
+
+        public void InSecond()
+        {
+            Console.WriteLine(this.hour * 3600 + this.minute * 60 + this.second);
+        }
+
+        public int ToSecond()
+        {
+            return this.hour * 3600 + this.minute * 60 + this.second;
+        }
+
+        public override string ToString()
+        {
+            var strh = "";
+            var strm = "";
+            var strs = "";
+            if (this.hour < 10) strh = $"0{this.hour}";
+            else strh = $"{this.hour}";
+            if (this.minute < 10) strm = $"0{this.minute}";
+            else strm = $"{this.minute}";
+            if (this.second < 10) strs = $"0{this.second}";
+            else strs = $"{this.second}";
+            return strh + ":" + strm + ":" + strs + "\n";
+
+             
+        }
+
+        public static Time operator +(Time f1, Time f2)
+        {
+            int s = f1.ToSecond() + f2.ToSecond();
+
+
+            return new Time(s);
+        }
+
+        public static Time operator -(Time f1, Time f2)
+        {
+            int s = f1.ToSecond() - f2.ToSecond();
+            if (s <= 0) return new Time(0);
+            return new Time(s);
+        }
+
+        public static Time operator *(Time f1, int a)
+        {
+            var s = f1.ToSecond() * a;
+            return new Time(s);
+        }
+
+        public static Time operator /(Time f1, int a)
+        {
+            if (a == 0)
+            {
+                Console.WriteLine("На 0 делить нельзя");
+                return new Time(0);
+            }
+            var s = f1.ToSecond() / 2;
+            return new Time(s);
+        }
+
+        public static bool operator >(Time f1, Time f2)
+        {
+            int s1 = f1.ToSecond();
+            int s2 = f2.ToSecond();
+            return s1 > s2;
+        }
+
+        public static bool operator <(Time f1, Time f2)
+        {
+            int s1 = f1.ToSecond();
+            int s2 = f2.ToSecond();
+            return s1 < s2;
+        }
+
+        public static bool operator <=(Time f1, Time f2)
+        {
+            int s1 = f1.ToSecond();
+            int s2 = f2.ToSecond();
+            return s1 <= s2;
+        }
+
+        public static bool operator >=(Time f1, Time f2)
+        {
+            int s1 = f1.ToSecond();
+            int s2 = f2.ToSecond();
+            return s1 >= s2;
+        }
+
+        public static bool operator ==(Time f1, Time f2)
+        {
+            int s1 = f1.ToSecond();
+            int s2 = f2.ToSecond();
+            return s1 == s2;
+        }
+
+        public static bool operator !=(Time f1, Time f2)
+        {
+            int s1 = f1.ToSecond();
+            int s2 = f2.ToSecond();
+            return s1 != s2;
+        }
+    }
+}
