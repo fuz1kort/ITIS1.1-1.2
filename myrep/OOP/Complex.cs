ba1cb5abf2282415809f5305b7fb535f9a601168
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2022-12-05 20:28:35 +0300

    init

---
+++ myrep/OOP/Complex.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Complexy
+    {
+        private double rez;
+        private double imz;
+
+        public Complexy(double rez, double imz)
+        {
+            this.rez = rez;
+            this.imz = imz;
+        }
+
+        public Complexy(double rez)
+        {
+            this.rez = rez;
+            this.imz = 0;
+        }
+        public Complexy()
+        {
+            this.rez = 0;
+            this.imz = 0;
+        }
+
+        public double Rez
+        {
+            get { return rez; }
+            set { rez = value; }
+        }
+        public double Imz
+        {
+            get { return imz; }
+            set { imz = value; }
+        }
+        public double Mod
+        {
+            get { return Math.Sqrt(rez * rez + imz * imz); }
+        }
+
+        public double Arg
+        {
+            get { return Math.Acos(rez / Mod); }
+        }
+
+        public void Add(Complexy comp1, Complexy comp2)
+        {
+            this.rez = comp1.rez + comp2.rez;
+            this.imz = comp1.imz + comp2.imz;
+        }
+        public void Minus(Complexy comp1, Complexy comp2)
+        {
+            this.rez = comp1.rez - comp2.rez;
+            this.imz = comp1.imz - comp2.imz;
+        }
+        public void Multy(Complexy comp1, Complexy comp2)
+        {
+            this.rez = comp1.rez * comp2.rez - comp1.imz * comp2.imz;
+            this.imz = comp1.imz * comp2.rez + comp2.imz * comp1.rez;
+        }
+        public Complexy Multy_1(Complexy comp1, Complexy comp2)
+        {
+            var rez = comp1.rez * comp2.rez + comp1.imz * comp2.imz;
+            var imz = comp1.imz * comp2.rez - comp2.imz * comp1.rez;
+            return new Complexy(rez, imz);
+        }
+
+
+        public string Div(Complexy comp1, Complexy comp2)
+        {
+            comp2.Imz = -comp2.Imz;
+            Complexy comp3 = Multy_1(comp1, comp2);
+            var numerator = comp3;
+            var denominator = comp2.Rez * comp2.Rez + comp2.Imz * comp2.Imz;
+            return $"({numerator})/{denominator}";
+        }
+
+        public override string ToString()
+        {
+            if (this.Imz == 0) { return $"{Rez}"; }
+            if (this.Rez == 0) { return $"{Imz}i"; }
+            if (this.Imz < 0) { return $"{Rez} - {-Imz}i"; }
+            return $"{Rez} + {Imz}i";
+        }
+    }
+}
--- myrep/OOP/Program.cs
+++ myrep/OOP/Program.cs
@@ -1,52 +1,20 @@
-﻿namespace FractionApp
+﻿using System.Security.Cryptography;
+
+namespace OOP
 {
 
     public class Program
     {
         public static void Main()
         {
-            //try
-            //{
-            Fraction f1 = new Fraction(1, 2);
-            Fraction f2 = new Fraction(32);
-            Fraction f3 = new Fraction();
-
-            f1.Numerator = 3;
-            //f1.SetDenom(2);
-
-
-            Fraction.StaticMethod();
-
-
-            //f1.Print();
-            Console.WriteLine(f1);
-
-
-            //f2.SetNum(2);
-            f2.Denominator = 1;
-
-            //Console.WriteLine($"Fraction - {f2.Numerator}/{f2.Denominator}");
-            Console.WriteLine($"Fraction - {f2}");
-
-
-            //f3.Print();
-            Console.WriteLine(f3);
-
-            f1.Add(f2);
-            //f1.Print();
-            Console.WriteLine(f1);
-
-            f3.Add(f3, f2);
-            //f3 = f3 + f2;
-            //f3.Print();
-            Console.WriteLine(f3);
-
-            //}
-            //catch(Exception ex)
-            //{
-            //    Console.WriteLine(ex.Message);
-            //}
-
+            Complexy s = new Complexy(1, 2);
+            Complexy x = new Complexy(3, 4);
+            Complexy a = new Complexy();
+            a.Add(s,x);
+            Console.WriteLine(a);
+            Console.WriteLine(a.Mod);
+            Console.WriteLine(a.Arg);
+            Console.WriteLine(a.Div(s, x));
         }
 
     }
