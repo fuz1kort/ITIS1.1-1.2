4e34f8508afd3a855812a12bbc832029591887b6
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2022-12-16 17:26:05 +0300

    vehicle

---
+++ MyVehicle/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVehicle
+{
+    public class Vehicle
+    {
+        protected readonly int maxSpeed;
+        protected int currSpeed;
+
+        int CurrSpeed
+        {
+            get => currSpeed;
+            set
+            {
+                currSpeed = value;
+                if (value > maxSpeed) currSpeed = maxSpeed;
+                if (value < 0) currSpeed = 0;
+            }
+        }
+
+        Vehicle() { }
+        Vehicle(int maxSpeed, int currSpeed)
+        {
+            this.maxSpeed = maxSpeed;
+            CurrSpeed = currSpeed;
+        }
+
+        public void SpeedUp(int speed) => CurrSpeed += speed;
+        public void SpeedDown(int speed) => CurrSpeed -= speed;
+    }
+}
