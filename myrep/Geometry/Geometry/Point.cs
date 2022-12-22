09664ed2d4a36d4296b32c94809173fe6f4219ba
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2022-12-22 14:35:24 +0300

    Geometry v1.2

--- myrep/Geometry/Geometry/Point.cs
+++ myrep/Geometry/Geometry/Point.cs
@@ -10,6 +10,7 @@ namespace Geometry
     {
         private double x;
         private double y;
+       public Point() { }
         public double X { get => x; set { x = Math.Round(value, 5); } }
         public double Y { get => y; set { y = Math.Round(value, 5); } }
         public Point(double xs, double ys)
@@ -17,15 +18,19 @@ namespace Geometry
             X = xs;
             Y = ys; 
         }
+
+        public static double GetRotatedX(double x, double y, double angle) => x * Math.Cos(angle) - y * Math.Sin(angle);
+        public static double GetRotatedY(double x, double y, double angle) => x * Math.Sin(angle) + y * Math.Cos(angle);
         public void Move(double xm, double ym)
         {
             X += xm;
             Y += ym;
         }
+
         public void Rotate(double angle) 
         {
-            var xr = Rectangle.GetRotatedX(X,Y,angle);
-            Y = Rectangle.GetRotatedY(X,Y,angle);
+            var xr = GetRotatedX(X,Y,angle);
+            Y = GetRotatedY(X,Y,angle);
             X = xr;
         }
 
--- myrep/Geometry/Geometry/Triangle.cs
+++ myrep/Geometry/Geometry/Triangle.cs
@@ -8,40 +8,28 @@ namespace Geometry
 {
     public class Triangle : IRotatable, IMovable
     {
-        public Point Point1 { get; set; }
-        public Point Point2 { get; set; }
-        public Point Point3 { get; set; }
+        public Point Point1 { get; set; } = new();
+        public Point Point2 { get; set; } = new();
+        public Point Point3 { get; set; } = new();
         public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
         {
-            Point1.X = x1;
-            Point1.Y = y1;
-            Point2.X = x2;
-            Point2.Y = y2;
-            Point3.X = x3;
-            Point3.Y = y3;
+            Point1 = new Point(x1, y1);
+            Point2 = new Point(x2, y2);
+            Point3 = new Point(x3, y3);
         }
 
         public void Move(double x, double y)
         {
-            Point1.X += x;
-            Point1.Y += y;
-            Point2.X += x;
-            Point2.Y += y;
-            Point3.X += x;
-            Point3.Y += y;
+            Point1.Move(x, y);
+            Point2.Move(x, y);
+            Point3.Move(x, y);
         }
 
         public void Rotate(double angle)
         {
-            var xr1 = Rectangle.GetRotatedX(Point2.X, Point2.Y, angle);
-            Point1.Y = Rectangle.GetRotatedY(Point1.X, Point1.Y, angle);
-            Point1.X = xr1;
-            var xr2 = Rectangle.GetRotatedX(Point2.X, Point2.Y, angle);
-            Point2.Y = Rectangle.GetRotatedY(Point2.X, Point2.Y, angle);
-            Point2.X = xr2;
-            var xr3 = Rectangle.GetRotatedX(Point3.X, Point3.Y, angle);
-            Point3.Y = Rectangle.GetRotatedY(Point3.X,Point3.Y, angle);
-            Point3.X = xr3;
+            Point1.Rotate(angle);
+            Point2.Rotate(angle);
+            Point3.Rotate(angle);
         }
 
         public override string ToString() => $"Координаты треугольника:\n({Point1.X},{Point1.Y})\n({Point2.X},{Point2.Y})\n({Point3.X},{Point3.Y})";
