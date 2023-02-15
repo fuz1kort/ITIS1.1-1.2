aa55d99b5be9c725594b23627bb09cd04fc57084
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-02-15 22:37:26 +0300

    init

--- myrep/2sem/2sem/Program.cs
+++ myrep/2sem/2sem/Program.cs
@@ -4,7 +4,14 @@
     {
         public void Main()
         {
-
+            MyStack stack = new MyStack(10);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            stack.Push(4);
+            stack.Push(2);
+            Console.WriteLine(stack.GetMax());
         }    
     }
 }
