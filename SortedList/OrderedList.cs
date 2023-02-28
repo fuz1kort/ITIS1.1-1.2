5f53b7ee7cf7a548487f28224ba7a808dec3d23e
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-02-28 10:02:44 +0300

    s

---
+++ SortedList/OrderedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedList
+{
+    public class OrderedList
+    {
+        private readonly LinkedList<int> list = new();
+
+        public OrderedList() { }
+
+        public void Add(int value)
+        {
+            if(list == null)
+                list.AddFirst(value);
+            if (list.First >= value)
+                list.AddFirst(value);
+            foreach(int item in list)
+            {
+                if (item <= value)
+                    list.AddAfter(list.Find(item), value);
+            }
+        }
+
+        public void Delete(int value)
+        {
+            if (list.Contains(value))
+                list.Remove(value);
+            else
+                throw new ArgumentException("Такого числа нет в списке");
+        }
+
+        public void Merge(OrderedList orderedlist)
+        {
+            var item = orderedlist.list.First;
+            if (item == null)
+                return;
+            while(item != null)
+            {
+                this.Add(item.Value);
+                item = item.Next;
+            }
+        }
+
+        public List<int> GetList()
+        {
+            List<int> newlist = new();
+            var item = list.First;
+            if (item == null)
+                return null;
+            while (item != null)
+            {
+                newlist.Add(item.Value);
+                item = item.Next;
+            }
+            return newlist;
+        }
+    }
+}
---
+++ SortedList/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SortedList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            OrderedList list1 = new();
+            list1.Add(1);
+            list1.Add(100);
+            list1.Add(5);
+            PrintList(list1);
+            OrderedList list2 = new();
+            list2.Add(2);
+            list2.Add(400);
+            list1.Merge(list2);
+            PrintList(list2);
+
+        }
+
+        public static void PrintList(OrderedList list)
+        {
+            var printlist = list.GetList();
+            printlist.ForEach(i => Console.WriteLine(i));
+        }
+    }
+}
