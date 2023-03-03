81fbc8323d0fcf54552dffa93ad394c957e401fc
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-03-03 13:44:59 +0300

    init

--- SortedList/OrderedList.cs
+++ SortedList/OrderedList.cs
@@ -3,52 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SortedList
 {
-    public class OrderedList
+    public class OrderedList<T> where T: IComparable
     {
-        private readonly LinkedList<int> list = new();
+        private class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; }
+            public Node(T value) => Value = value;
+        }
+
+        private Node head;
 
         public OrderedList() { }
 
-        public void Add(int value)
+        public void Add(T value)
         {
-            if(list == null)
-                list.AddFirst(value);
-            if (list.First >= value)
-                list.AddFirst(value);
-            foreach(int item in list)
+            Node newelem = new(value);
+            if (head == null)
             {
-                if (item <= value)
-                    list.AddAfter(list.Find(item), value);
-            }
-        }
-
-        public void Delete(int value)
-        {
-            if (list.Contains(value))
-                list.Remove(value);
-            else
-                throw new ArgumentException("Такого числа нет в списке");
-        }
-
-        public void Merge(OrderedList orderedlist)
-        {
-            var item = orderedlist.list.First;
-            if (item == null)
+                head = newelem;
                 return;
-            while(item != null)
+            }
+
+            if (newelem.Value.CompareTo(head.Value) < 0)
             {
-                this.Add(item.Value);
-                item = item.Next;
+                newelem.Next = head;
+                head = newelem;
+            }
+            else
+            {
+                Node current = head;
+
+                while (current.Next != null && current.Next.Value.CompareTo(newelem.Value) < 0)
+                {
+                    current = current.Next;
+                }
+                newelem.Next = current.Next;
+                current.Next = newelem;
             }
         }
 
-        public List<int> GetList()
+        public void Delete(T value)
         {
-            List<int> newlist = new();
-            var item = list.First;
+            var elem = head;
+            var prev = elem;
+            if (head == null)
+                return;
+            while (elem != null)
+            {
+                if (elem.Value.CompareTo(value)==0)
+                {
+                    prev.Next = elem.Next;
+                    if (head.Value.CompareTo(value) == 0)
+                        head = head.Next;
+                    break;
+                }
+                prev = elem;
+                elem = elem.Next;
+            }
+        }
+
+        public void Merge(OrderedList<T> orderedlist)
+        {
+            if (orderedlist == null)
+                return;                
+            Node curr = head;
+            Node curr2 = orderedlist.head;
+            Node prev = null;
+
+            while(curr != null || curr2 != null)
+            {
+                if (curr2.Value.CompareTo(curr.Value)<0)
+                {
+                    var temp = curr2;
+                    curr2 = curr2.Next;
+                    if(prev == null)
+                    {
+                        temp.Next = head;
+                        head = temp;
+                        prev = temp;
+                    }
+
+                    else
+                    {
+                        prev.Next = temp;
+                        temp.Next = curr;
+                        prev = temp;
+                    }
+                }
+
+                else
+                {
+                    if (curr.Next != null)
+                    {
+                        prev = curr;
+                        curr = curr.Next;
+                    }
+                    else break;
+                }
+            }
+
+            if (curr2 != null)
+                prev.Next = curr2;
+        }
+
+        public List<T> GetList()
+        {
+            List<T> newlist = new() { head.Value };
+            var item = head.Next;
             if (item == null)
                 return null;
             while (item != null)
--- SortedList/Program.cs
+++ SortedList/Program.cs
@@ -4,23 +4,28 @@
     {
         public static void Main()
         {
-            OrderedList list1 = new();
+            OrderedList<int> list1 = new();
             list1.Add(1);
             list1.Add(100);
             list1.Add(5);
             PrintList(list1);
-            OrderedList list2 = new();
+            list1.Delete(100);
+            PrintList(list1);
+            OrderedList<int> list2 = new();
             list2.Add(2);
             list2.Add(400);
-            list1.Merge(list2);
             PrintList(list2);
-
+            list1.Merge(list2);
+            PrintList(list1);
+            list1.Add(83);
+            PrintList(list1);
         }
 
-        public static void PrintList(OrderedList list)
+        public static void PrintList(OrderedList<int> list)
         {
             var printlist = list.GetList();
             printlist.ForEach(i => Console.WriteLine(i));
+            Console.WriteLine("-----");
         }
     }
 }
