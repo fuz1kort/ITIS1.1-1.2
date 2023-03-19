0c8a611b7650715cb2c542a42c6a47c74c9dd8ed
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-03-19 12:59:55 +0300

    new myset

---
+++ myrep/OpenSet/OpenSet/MySet.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSet
+{
+    public class MySet
+    {
+        public Hashtable hashtable = new Hashtable();
+
+        private int count = 0;
+
+        public MySet() { }
+
+        public bool IsContains(int s) => (this.hashtable.ContainsValue(s));
+
+        public void Add(int s)
+        {
+            if (IsContains(s))
+                return;
+            hashtable.Add(++count, s);
+        }
+
+        public void Delete(int s)
+        {
+            if (!IsContains(s))
+                return;
+            hashtable.Remove(s);
+            count--;
+        }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+            foreach (int s in hashtable.Values)
+                str.Append($"{s} ");
+            return str.ToString();
+        }
+
+        public int[] ToArray()
+        {
+            var array = new int[count];
+            var i = 0;
+            foreach (int s in hashtable.Values)
+                array[i++] = s;
+            return array;
+        }
+
+        public MySet Intersection(MySet set)
+        {
+            var newset = new MySet();
+            foreach (int s in set.hashtable.Values)
+                if (hashtable.ContainsValue(s))
+                    newset.Add(s);
+            return newset;
+        }
+
+        public MySet Unification(MySet set)
+        {
+            var newset = new MySet();
+            foreach(int s1 in hashtable.Values)
+                newset.Add(s1);
+            foreach (int s2 in set.hashtable.Values)
+                newset.Add(s2);
+            return newset;
+        }
+
+        public void Difference(MySet set)
+        {
+            foreach(int s in set.hashtable.Values)
+            {
+                if(hashtable.ContainsValue(s))
+                    hashtable.Remove(s);
+            }
+        }
+
+        public MySet SymDifference(MySet set)
+        {
+            var newset = new MySet();
+            foreach(int s in set.hashtable.Values)
+            {
+                if (!hashtable.ContainsValue(s))
+                    newset.Add(s);
+            }
+
+            foreach(int s in hashtable.Values)
+            {
+                if(!set.hashtable.ContainsValue(s))
+                    newset.Add(s);
+            }
+
+            return newset;
+
+        }
+
+        public bool IsSupSet(MySet set)
+        {
+            foreach(int s in hashtable.Values)
+            {
+                if (!set.hashtable.ContainsValue(s))
+                    return false;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ myrep/OpenSet/OpenSet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.Design;
+
+namespace OpenSet
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("set1, IsContains, ToString");
+            var set1 = new MySet();
+            set1.Add(1);
+            set1.Add(2);
+            set1.Add(3);
+            set1.Add(4);
+            set1.Add(2);
+            set1.Delete(2);
+            Console.WriteLine(set1);
+            Console.WriteLine(set1.IsContains(1));
+            Console.WriteLine("--------------");
+            Console.WriteLine("ToArray");
+            var arr = set1.ToArray();
+            foreach (var item in arr)
+                Console.Write($"{item} ");
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+            Console.WriteLine("set2");
+            var set2 = new MySet();
+            set2.Add(1);
+            set2.Add(8);
+            set2.Add(15);
+            Console.WriteLine(set2);
+            Console.WriteLine("--------------");
+            Console.WriteLine("set 1 intersection set 2");
+            Console.WriteLine(set1.Intersection(set2));
+            Console.WriteLine("--------------");
+            Console.WriteLine("set 1 unification set 2");
+            Console.WriteLine(set1.Unification(set2));
+            Console.WriteLine("--------------");
+            Console.WriteLine("set 1 difference set 2");
+            set1.Difference(set2);
+            Console.WriteLine(set1);
+            Console.WriteLine("--------------");
+            Console.WriteLine("set 1 is supset set 2");
+            set2 = set1.Unification(set2);
+            Console.WriteLine(set1);
+            Console.WriteLine(set2);
+            Console.WriteLine(set1.IsSupSet(set2));
+        }
+    }
+}
