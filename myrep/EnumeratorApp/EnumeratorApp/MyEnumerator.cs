9ef98510b7b4186b79f5bf0e756566059d99293c
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-03-24 09:57:06 +0300

    wordenum

--- myrep/EnumeratorApp/EnumeratorApp/MyEnumerator.cs
+++ myrep/EnumeratorApp/EnumeratorApp/MyEnumerator.cs
@@ -1,37 +1,45 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
+using YieldApp;
 
-namespace YieldApp
+namespace YieldApp;
+
+public class StringEnumerator : IEnumerable<string>
 {
-    public class ListEnumerator : IEnumerator
+    private string[] Words;
+    public WordsEnumerator WordsOrderedByLength;
+    private bool IfOrdered = false;
+
+    public StringEnumerator(string words)
     {
-
-        List<string> lst;
-        int cur;
-
-        public ListEnumerator(List<string> lst)
-        {
-            this.lst = lst;
-            cur = -1;
-        }
-
-
-        public string Current => lst[cur];
-
-        object IEnumerator.Current => throw new NotImplementedException();
-
-
-
-        public bool MoveNext()
-        {
-            cur++;
-            return cur < lst.Count;
-        }
-
-        public void Reset() => cur = -1;
+        Words = words.Split(' ');
     }
+    public IEnumerator<string> GetEnumerator()
+    {
+        foreach (var word in Words)
+        {
+            yield return word;
+        }
+    }
+
+    public IEnumerable<string> GetOrderedByLength()
+    {
+        if (!IfOrdered)
+        {
+            WordsOrderedByLength = new WordsEnumerator(Words);
+            IfOrdered = true;
+        }
+
+        return WordsOrderedByLength;
+
+    }
+
+    public IEnumerable GetOrderedWords()
+    {
+        foreach (var word in Words)
+        {
+            yield return word;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
---
+++ myrep/EnumeratorApp/EnumeratorApp/WordsEnumerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+namespace YieldApp;
+
+public class WordsEnumerator : IEnumerable<string>
+{
+    public readonly string[] Words;
+
+    public WordsEnumerator(string[] words)
+    {
+        Words = new string[words.Length];
+        for (int i = 0; i < words.Length; i++)
+        {
+            Words[i] = words[i];
+        }
+        Sort();
+
+    }
+
+    private void Sort()
+    {
+        for (int i = 0; i < Words.Length; i++)
+        {
+            for (int e = i; e < Words.Length; e++)
+            {
+                if (Words[i].Length > Words[e].Length)
+                    (Words[i], Words[e]) = (Words[e], Words[i]);
+            }
+        }
+    }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        foreach (var word in Words)
+        {
+            yield return word;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
