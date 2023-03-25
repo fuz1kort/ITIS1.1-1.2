46b4a5e8f519c5d92ca4d3543d262e455e8bb616
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-03-25 18:51:29 +0300

    grahamscansets

--- myrep/Semestrovka№1/Semestrovka№1/Program.cs
+++ myrep/Semestrovka№1/Semestrovka№1/Program.cs
@@ -7,66 +7,57 @@ namespace Semestr1
         public static void Main()
         {
             FileInfo file;
+            int pcount = 0;
             var r = new Random();
-            int pointcount = 0;
             for (int i = 0; i < 100; i++)
             {
-                pointcount += 100;
+                pcount += 100;
                 file = new FileInfo($@"C:\MySet\Set{i + 1}.txt");
                 var stringpoint = new StringBuilder();
-                for (int j = 0; j < pointcount; j++)
+                for (int j = 0; j < pcount; j++)
                 {
-                    stringpoint.AppendLine($"{r.Next(-1000, 1000)}, {r.Next(-1000, 1000)}");
+                    stringpoint.AppendLine($"{r.Next(-100, 100)}, {r.Next(-100, 100)}");
                 }
-                
-                using var sw = file.AppendText();
+
+                using (var sw = file.AppendText())
                 {
                     sw.WriteLine(stringpoint.ToString());
                 }
-                
             }
 
             //var r = new Random();
-            //string[] Points;
-            //List<string[]> listofsets = new List<string[]>();
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    Points = File.ReadAllLines($@"C:\MySet\Set{i + 1}.txt");
-            //    listofsets.Add(Points);
-            //}
-            //var set1 = listofsets[5];
-            //List<Point> points = new();
-            //foreach (var p in set1)
-            //{
-            //    if (p == "")
-            //        break;
-            //    points.Add(new Point(Convert.ToInt32((p.Split(",")[0])), Convert.ToInt32((p.Split(",")[1]))));
-            //}
-
-            //foreach (var poi in points)
-            //{
-            //    Console.WriteLine(poi);
-            //}
-
-
+            string[] Points;
+            List<string[]> listofsets = new List<string[]>();
+            for (int i = 0; i < 100; i++)
+            {
+                Points = File.ReadAllLines($@"C:\MySet\Set{i + 1}.txt");
+                listofsets.Add(Points);
+            }
+            var set1 = listofsets[99];
+            List<Point> points = new();
+            foreach (var p in set1)
+            {
+                if (p != "")
+                {
+                    points.Add(new Point(Convert.ToInt32((p.Split(",")[0])), Convert.ToInt32((p.Split(",")[1]))));
+                }
+                else break;
+            }
 
             //List<Point> points = new List<Point>
             //{
             //    new Point(0, 3),
             //    new Point(2, 2),
-            //    new Point(3, 0),
-            //    new Point(2, 1),
-            //    new Point(0, -1),
-            //    new Point(0, 0),
             //    new Point(-1, 1),
-            //    new Point(1, 1),
-            //    new Point(1,0),
-            //    new Point(-2, 0)
+            //    new Point(2, 1),
+            //    new Point(-300, 0),
+            //    new Point(0, 0),
+            //    new Point(3, -3)
             //};
 
-            //var list = GrahamScan.ConvexHull(points);
-            //foreach (var item in list)
-            //    Console.WriteLine($"({item.X},{item.Y})");
+            var list = GrahamScan.ConvexHull(points);
+            foreach (var item in list)
+                Console.WriteLine($"({item.X},{item.Y})");
         }
     }
 }
