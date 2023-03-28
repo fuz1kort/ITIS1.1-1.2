773d4126a5bbb323ce470019b224205f1f3bff85
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-03-28 09:02:45 +0300

    stringenum

--- myrep/EnumeratorApp/EnumeratorApp/StringEnumerator.cs
+++ myrep/EnumeratorApp/EnumeratorApp/StringEnumerator.cs
@@ -6,7 +6,7 @@ namespace YieldApp;
 public class StringEnumerator : IEnumerable<string>
 {
     private string[] Words;
-    public WordsEnumerator WordsOrderedByLength;
+    //public WordsEnumerator WordsOrderedByLength;
     private bool IfOrdered = false;
 
     public StringEnumerator(string words)
@@ -15,31 +15,23 @@ public class StringEnumerator : IEnumerable<string>
     }
     public IEnumerator<string> GetEnumerator()
     {
-        foreach (var word in Words)
+        foreach (var word in Words.OrderBy(p => p.Length))
+        //foreach(var word in Words)
         {
             yield return word;
         }
     }
 
-    public IEnumerable<string> GetOrderedByLength()
-    {
-        if (!IfOrdered)
-        {
-            WordsOrderedByLength = new WordsEnumerator(Words);
-            IfOrdered = true;
-        }
+    //public IEnumerable<string> GetOrderedByLength()
+    //{
+    //    if (!IfOrdered)
+    //    {
+    //        WordsOrderedByLength = new WordsEnumerator(Words);
+    //        IfOrdered = true;
+    //    }
 
-        return WordsOrderedByLength;
-
-    }
-
-    public IEnumerable GetOrderedWords()
-    {
-        foreach (var word in Words)
-        {
-            yield return word;
-        }
-    }
+    //    return WordsOrderedByLength;
+    //}
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
