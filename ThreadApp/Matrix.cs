9ab51b183e6e8b07fd6166b23791e125b7dec7f3
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-04-20 13:38:37 +0300

    threadmatrix work

---
+++ ThreadApp/Matrix.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace ThreadApp
+{
+    public class Matrix
+    {
+
+        int[,] matrix;
+        public int size;
+
+
+        public Matrix(int n)
+        {
+            size = n;
+            matrix = new int[size, size];
+            Random r = new Random();
+
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    matrix[i, j] = r.Next(-10000, 10000);
+        }
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                    sb.Append($" {matrix[i, j]}");
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        public int[] PoslMins()
+        {
+            int[] res = new int[size];
+            Array.Fill(res, int.MaxValue);
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    res[i] = matrix[i, j] < res[i] ? matrix[i, j] : res[i];
+
+            return res;
+
+        }
+
+
+
+        public int[] ParallelMins()
+        {
+            object locker = new object();
+            int[] res = new int[size];
+
+            Parallel.For(0, size, i =>
+            {
+                int min = ParMinRow(i);
+
+                lock (locker)
+                {
+                    res[i] = min;
+                }
+
+            });
+
+            return res;
+
+        }
+
+
+        public int ParMinRow(int row)
+        {
+            int pmin = int.MaxValue;
+            for (int j = 0; j < size; j++)
+                pmin = matrix[row, j] < pmin ? matrix[row, j] : pmin;
+            return (pmin);
+        }
+    }
+}
--- ThreadApp/Program.cs
+++ ThreadApp/Program.cs
@@ -6,65 +6,20 @@ namespace ThreadApp
     {
         static void Main()
         {
-            var r = new Random();
             Console.WriteLine("Введите размер матрицы");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[,] matrix = new int[n,n];
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    matrix[i,j] = r.Next(-100, 100);
-                }
-            }
+            var m = new Matrix(Convert.ToInt32(Console.ReadLine()));
 
-            Console.WriteLine("Введите количество потоков");
-            var k = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i < k; i++)
-            {
-                Thread thread = new(Work);
-                thread.Start();
-            }
-
-            void Work() => Console.WriteLine($"Hello {Thread.CurrentThread.ManagedThreadId}");
-
-            object locker = new();
-            var maxmatrix = int.MinValue;
-            var minmatrix = int.MaxValue;
-            var t1 = Stopwatch.StartNew();
-
-            string MinMax(int [] row)
-            {
-                var min = int.MaxValue;
-                var max = int.MinValue;
-                for (int i = 0; i < n; i++)
-                {
-                    max = row[i] > max ? row[i] : max;
-                    min = row[i] < min ? row[i] : min;
-                }
-                return $"{max} {min}";
-            }
+            var t1 = DateTime.Now;
+            Console.WriteLine(m.PoslMins().Max());
+            var t2 = DateTime.Now;
+            Console.WriteLine($"последовательно - {t2 - t1}");
 
 
 
-            //Parallel.For(0, n, i =>
-            //{
-            //    int min1 = int.MaxValue;
-            //    int max1 = int.MinValue;
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        if (matrix[i, j] > max1)
-            //            max1 = matrix[i, j];
-            //        if (matrix[i, j] < min1)
-            //            min1 = matrix[i, j];
-            //    }
-
-            t1.Stop();
-            Console.WriteLine($"Max = {maxmatrix}; " +
-                              $"Min = {minmatrix}." +
-                              $"Time = {t1.Elapsed}");
-
-
+            var t11 = DateTime.Now;
+            Console.WriteLine(m.ParallelMins().Max());
+            var t12 = DateTime.Now;
+            Console.WriteLine($"потоков {m.size}; время - {t12 - t11}");
         }
     }
 }
